cecfe54fc1f23666cd93d2b07b24480c3304119c
sarseniy <senokosov.ao@phystech.edu>
2020-10-21 00:38:26 +0300

    Minor fixes

--- Vector.py
+++ Vector.py
@@ -1,21 +1,15 @@
 class Vector:
-    def __init__(self, x, y, *args):
+    def __init__(self, x, y, z=None):
         self.x = x
         self.y = y
-        self.z = None
-        for arg in args:
-            self.z = arg
-        if self.z is None:
-            self.three_dim = False
-        else:
-            self.three_dim = True
+        self.z = z
 
     def __repr__(self):
         """
         Магический метод, вызываемый при отображении информации об объекте
         Должен вернуть строку
         """
-        if not self.three_dim:
+        if self.z is None:
             return f'Vector({self.x}, {self.y})'
         else:
             return f'Vector({self.x}, {self.y}, {self.z})'
@@ -30,10 +24,7 @@ class Vector:
         >>> Vector(1, 2) == Vector(2, 1)
         False
         """
-        if self.three_dim:
-            return self.x == other.x and self.y == other.y and self.z == other.z
-        else:
-            return self.x == other.x and self.y == other.y
+        return self.x == other.x and self.y == other.y and self.z == other.z
 
     def __add__(self, other):
         """
@@ -46,16 +37,16 @@ class Vector:
         Vector(6, 7)
         """
         if isinstance(other, Vector):
-            if self.three_dim and other.three_dim:
+            if not (self.z is None) and not (other.z is None):
                 return Vector(self.x + other.x, self.y + other.y, self.z + other.z)
-            elif not self.three_dim and not other.three_dim:
+            elif self.z is None and other.z is None:
                 return Vector(self.x + other.x, self.y + other.y)
             else:
                 raise TypeError(
                     f'cannot add 3d Vector to 2d'
                 )
         elif isinstance(other, (int, float)):
-            if self.three_dim:
+            if not (self.z is None):
                 return Vector(self.x + other, self.y + other, self.z + other)
             else:
                 return Vector(self.x + other, self.y + other)
@@ -75,16 +66,16 @@ class Vector:
         Vector(0, 1)
         """
         if isinstance(other, Vector):
-            if self.three_dim and other.three_dim:
+            if not (self.z is None) and not (other.z is None):
                 return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
-            elif not self.three_dim and not other.three_dim:
+            elif self.z is None and other.z is None:
                 return Vector(self.x - other.x, self.y - other.y)
             else:
                 raise TypeError(
                     f'cannot substitute 3d Vector from 2d'
                 )
         elif isinstance(other, (int, float)):
-            if self.three_dim:
+            if not (self.z is None):
                 return Vector(self.x - other, self.y - other, self.z - other)
             else:
                 return Vector(self.x - other, self.y - other)
@@ -101,16 +92,16 @@ class Vector:
         Vector(5, 10)
         """
         if isinstance(other, Vector):
-            if self.three_dim and other.three_dim:
+            if not (self.z is None) and not (other.z is None):
                 return self.x * other.x + self.y * other.y + self.z * other.z
-            elif not self.three_dim and other.three_dim:
+            elif self.z is None and other.z is None:
                 return self.x * other.x + self.y * other.y
             else:
                 raise TypeError(
                     f'cannot multiply 3d Vector by 2d'
                 )
         elif isinstance(other, (int, float)):
-            if self.three_dim:
+            if not (self.z is None):
                 return Vector(self.x * other, self.y * other, self.z * other)
             else:
                 return Vector(self.x * other, self.y * other)
@@ -127,7 +118,7 @@ class Vector:
         Vector(0.2, 0.4)
         """
         if isinstance(other, (int, float)):
-            if self.three_dim:
+            if not (self.z is None):
                 return Vector(self.x / other, self.y / other, self.z / other)
             else:
                 return Vector(self.x / other, self.y / other)
@@ -144,7 +135,7 @@ class Vector:
         >>> -Vector(0, 0)
         Vector(0, 0)
         """
-        if self.three_dim:
+        if not (self.z is None):
             return Vector(-self.x, -self.y, -self.z)
         else:
             return Vector(-self.x, -self.y)
@@ -157,7 +148,7 @@ class Vector:
         >>> abs(Vector(3, 4))
         5.0
         """
-        if self.three_dim:
+        if not (self.z is None):
             return (self.x ** 2 + self.y ** 2 + self.z ** 2) ** 0.5
         else:
             return (self.x ** 2 + self.y ** 2) ** 0.5
@@ -168,7 +159,7 @@ class Vector:
         :return: Векторное произведение двух векторов
         """
         if isinstance(other, Vector):
-            if self.three_dim and other.three_dim:
+            if not (self.z is None) and not (other.z is None):
                 tx = self.y * other.z - self.z * other.y
                 ty = -self.x * other.z + self.z * other.x
                 tz = self.x * other.y - self.y * other.x
@@ -183,50 +174,34 @@ class Vector:
             )
 
 
-def area(first, other_1, *args):
+def area(first, other_1,):
     """
     Функция считает площадь параллелограмма, натянутого на пару векторов
     """
-    other_2 = None
-    for arg in args:
-        other_2 = arg
-    if not (other_2 is None):
-        raise TypeError(
-            'cannot calculate area of 3 and more Vectors'
-        )
-    else:
-        if isinstance(other_1, Vector):
-            if first.three_dim and other_1.three_dim:
-                return abs(first @ other_1)
-            elif not first.three_dim and not other_1.three_dim:
-                return abs(first.x * other_1.y - other_1.x * first.y)
-            else:
-                raise TypeError(
-                    'cannot calculate area of Vectors from different dimensions'
-                )
+    if isinstance(other_1, Vector):
+        if not (first.z is None) and not (other_1.z is None):
+            return abs(first @ other_1)
+        elif first.z is None and other_1.z is None:
+            return abs(first.x * other_1.y - other_1.x * first.y)
         else:
             raise TypeError(
-                f'cannot calculate area of {other_1.__class__.__name__} and Vector'
+                'cannot calculate area of Vectors from different dimensions'
             )
-
-
-def volume(first, other_1, other_2, *args):
-    other_3 = None
-    for arg in args:
-        other_3 = arg
-    if not (other_3 is None):
-        raise TypeError(
-            'cannot calculate volume of 4 and more Vectors'
-        )
     else:
-        if isinstance(other_1, Vector) and isinstance(other_2, Vector):
-            return abs(first.x * (other_1.y * other_2.z - other_1.z * other_2.y) + \
-                       first.y * (-other_1.x * other_2.z + other_1.z * other_2.x) + \
-                       first.z * (other_1.x * other_2.y - other_1.y * other_2.x))
-        else:
-            raise TypeError(
-                f'cannot calculate volume of {other_1.__class__.__name__}, {other_2.__class__.__name__} and Vector'
-            )
+        raise TypeError(
+            f'cannot calculate area of {other_1.__class__.__name__} and Vector'
+        )
+
+
+def volume(first, other_1, other_2):
+    if isinstance(other_1, Vector) and isinstance(other_2, Vector):
+        return abs(first.x * (other_1.y * other_2.z - other_1.z * other_2.y) + \
+                   first.y * (-other_1.x * other_2.z + other_1.z * other_2.x) + \
+                   first.z * (other_1.x * other_2.y - other_1.y * other_2.x))
+    else:
+        raise TypeError(
+            f'cannot calculate volume of {other_1.__class__.__name__}, {other_2.__class__.__name__} and Vector'
+        )
 
 
 if __name__ == '__main__':
